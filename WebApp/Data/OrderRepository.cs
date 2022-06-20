3dc06cc943266e848ca987e70ecb1e90f8103a5c
Piotr Kałuziński <piotr.kaluzinski@student.wat.edu.pl>
2022-06-20 22:08:14 +0200

    Order Orders By Delivery Date

--- WebApp/Data/OrderRepository.cs
+++ WebApp/Data/OrderRepository.cs
@@ -14,7 +14,7 @@ namespace WebApp.Data
 
         public async Task<List<Order>> GetAllOrdersAsync()
         {
-            return await db.Orders.ToListAsync();
+            return await db.Orders.OrderByDescending(x => x.DeliveryDate).ToListAsync();
         }
 
         public async Task<List<OrderDetail>> GetOrderDetailsAsync(long orderId)
